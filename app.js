4f2ab6a63d9744701cdc145c6962951f3aa6b997
GuiSantosMaria <gsantosmaria07@gmail.com>
2025-05-27 17:54:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 function calcular() {
-    // Obtém os valores dos inputs
     let etanol = parseFloat(document.getElementById('etanol').value);
     let gasolina = parseFloat(document.getElementById('gasolina').value);
-
-    // Verifica se algum dos valores é inválido (NaN ou menor ou igual a zero)
     if (isNaN(etanol) || isNaN(gasolina) || etanol <= 0 || gasolina <= 0) {
         alert('Por favor, insira valores válidos e positivos para etanol e gasolina.');
         return;
     }
-
-    // Calcula a razão
+    
     let resultado = etanol / gasolina;
-
-    // Define a imagem e mensagem conforme a razão
+    
     let mensagem = `Resultado: ${resultado.toFixed(2)}<br>`;
     
     if (resultado <= 0.7) {
